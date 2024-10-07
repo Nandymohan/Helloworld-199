30360b78b0570e4de3360bbc92a69c0e7fbb27ad
Nandymohan <nandhinipsgc@gmail.com>
2024-10-07 14:38:12 +0530

    Update Jenkinshello.java

--- Jenkinshello.java
+++ Jenkinshello.java
@@ -2,7 +2,5 @@ public class Jenkinshello{
 public static void main(String[] args){
 System.out.println("Hello");
   System.out.println("World!");
-  System.out.println("Nandhini");
-  System.out.println("Nandhini Mohan");
-
+ 
 }}
