41ca172131c9a9e66c9c2a26a662a8b793eca88b
Nandymohan <nandhinipsgc@gmail.com>
2024-12-25 22:22:52 +0530

    Update moh.java

--- thambi/src/main/java/vasa/moh.java
+++ thambi/src/main/java/vasa/moh.java
@@ -6,6 +6,7 @@ public class moh {
 		// TODO Auto-generated method stub
 System.out.println("This is nandhini , creating maven pjoject from eclipse and pushing code to git");
 System.out.println("Now figured the pbm & fixed it");
+		System.out.println("You dont need to add java file in jenkins workspace");
 	}
 
 }
